e686a7b99e04f02327f2375723c11c724d2ee80a
Manoj Krishna <ext_mkrishna@falabella.cl>
2018-12-31 13:30:10 -0300

    remove relevant paths

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
-export const GET_EVENTS = 'http://127.0.0.1:8080/getEvent/';
+export const GET_EVENTS = '/getEvent/';
 
-export const POST_EVENT = "http://127.0.0.1:8080/postcomment";
+export const POST_EVENT = "/postcomment";
 
-export const POST_REPLY = "http://127.0.0.1:8080/postreply";
+export const POST_REPLY = "/postreply";
 
-export const GUEST_USER = "http://127.0.0.1:8080/user/guestRegister"
+export const GUEST_USER = "/user/guestRegister"
 
-export const ADD_LIKES = "http://127.0.0.1:8080/addLikes"
+export const ADD_LIKES = "/addLikes"
